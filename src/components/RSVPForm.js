a5fead742ccf957b2551d2b3498db2982821cbfe
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-08 22:10:16 -0500

    start putting together the RSVP form

--- src/components/RSVPForm.js
+++ src/components/RSVPForm.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const RSVPForm = () => (
-  <div>
-    <p>Form coming soon</p>
-  </div>
-);
+class RSVPForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      allergies: '',
+      additionalGuests: [],
+      attending: false,
+      mealPreference: '',
+      notes: '',
+      error: ''
+    };
+  }
+
+  onFirstNameChange = (e) => {
+    const firstName = e.target.value;
+    this.setState(() => ({ firstName }));
+  };
+
+  onLastNameChange = (e) => {
+    const lastName = e.target.value;
+    this.setState(() => ({ lastName }));
+  };
+
+  onEmailChange = (e) => {
+    const email = e.target.value;
+    this.setState(() => ({ email }));
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        {this.state.error && <p>{this.state.error}</p>}
+        <input
+          type="text"
+          placeholder="First name"
+          autoFocus
+          onChange={this.onFirstNameChange}
+        />
+        <input
+          type="text"
+          placeholder="Last name"
+          onChange={this.onLastNameChange}
+        />
+        <input
+          type="text"
+          placeholder="Email address"
+          onChange={this.onEmailChange}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  };
+}
 
 export default RSVPForm;
