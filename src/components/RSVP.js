77fce3901990f0d28ad0565f0b76cfd2ce3bec15
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-14 18:12:18 -0400

    try to fix base url again

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -5,7 +5,7 @@ import RSVPForm from './RSVPForm';
 
 export default class RSVP extends Component {
   onSubmit = (data) => {
-    axios.post(`${process.env.BASE_URL}/responses`, data)
+    axios.post(`${process.env.BASE_URL}responses`, data)
       .then((res) => {
         console.log(res.data);
         this.props.history.push('/');
