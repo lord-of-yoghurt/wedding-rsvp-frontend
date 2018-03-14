fec998842da444070efed6c724ca62984bf57ec5
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-14 14:03:40 -0400

    add form submission to external server

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -3,10 +3,15 @@ import axios from 'axios';
 
 import RSVPForm from './RSVPForm';
 
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+
 export default class RSVP extends Component {
   onSubmit = (data) => {
-    axios.post('http://localhost:3000/responses', data)
-      .then((res) => console.log(res))
+    axios.post(`${BASE_URL}/responses`, data)
+      .then((res) => {
+        console.log(res);
+        this.props.history.push('/');
+      })
       .catch((e) => console.log(e))
   };
 
