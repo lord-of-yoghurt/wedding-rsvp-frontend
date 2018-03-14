89b2f2c85f2fb04d9c123af0d66d4e5f02bc5173
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-14 14:45:49 -0400

    prepare for data submission test

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -9,7 +9,7 @@ export default class RSVP extends Component {
   onSubmit = (data) => {
     axios.post(`${BASE_URL}/responses`, data)
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
         this.props.history.push('/');
       })
       .catch((e) => console.log(e))
