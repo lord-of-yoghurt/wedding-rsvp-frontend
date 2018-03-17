058a1fe88913ea35b89c704176cbce8fcfa31ae6
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-17 07:52:17 -0400

    add favicon, start styling the homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => (
-  <div>
-    <h1>Haodong and David are getting married!</h1>
-    <p>Join the party! <Link to="/rsvp">RSVP</Link></p>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <p className="box-title">Haodong and David are getting married!</p>
+      <div className="box-title__box">
+        <div className="box-title__left">
+          <p>Blablabla where</p>
+          <p>Blablabla when</p>
+        </div>
+        <div className="box-title__right">
+          <img className="box-layout__img" src="/images/mt-fuji.png"></img>
+        </div>
+      </div>
+      <p className="box-subtitle">We look forward to sharing this special day with you!</p>
+      <div>
+        <Link className="app-button app-button--wide app-button--link" to="/rsvp">RSVP</Link>
+      </div>
+    </div>
   </div>
 );
 
