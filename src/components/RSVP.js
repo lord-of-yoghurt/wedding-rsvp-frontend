6b448052710e173755a47b02ffd673704a1f0ea8
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-21 17:54:02 -0400

    new fix for rsvp form height

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -8,21 +8,16 @@ export default class RSVP extends Component {
     axios.post(`${process.env.BASE_URL}responses`, data)
       .then((res) => {
         console.log(res.data);
-        this.props.history.push('/');
+        this.props.history.push('/thankyou', { attending: data.attending });
       })
       .catch((e) => console.log(e))
-  };
-
-  onSubmitTemp = (data) => {
-    console.log(data);
-    this.props.history.push('/thankyou', { attending: data.attending });
   };
 
   render() {
     return (
       <div className="form-wrapper">
         <div className="form-container">
-          <div className="form-container--left"><RSVPForm onSubmit={this.onSubmitTemp} /></div>
+          <div className="form-container--left"><RSVPForm onSubmit={this.onSubmit} /></div>
           <div className="form-container--right"></div>
         </div>
       </div>
