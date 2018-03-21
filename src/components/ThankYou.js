69f6d2a275a0681b22855197d55d186c814778aa
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-21 17:21:14 -0400

    style ThankYou page, make adjustments to homepage

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThankYou = (props) => {
   // throw back to home page if user attempts
@@ -11,23 +12,24 @@ const ThankYou = (props) => {
   const attending = props.location.state.attending;
 
   return (
-    <div>
-      <div>
-        <div>
-          <h2>Thank you!</h2>
-          {attending === 'yes' &&
-            <p>
-              We are so excited to see you at our wedding! Please check back for
-              detailed schedule and updated registry information. We will send out
-              a notification once there are updates.
-            </p>
-          }
-          {attending === 'no' &&
-            <p>
-              We are sorry to hear that you are not available.
-            </p>
-          }
-        </div>
+    <div className="thankyou-wrapper">
+      <div className="thankyou-container animated fadeInDown">
+        <h2>Thank you!</h2>
+        {attending === 'yes' &&
+          <p className="thankyou-paragraph">
+            We are so excited to see you at our wedding! Please check back for
+            detailed schedule and updated registry information. We will send out
+            a notification once there are updates.
+          </p>
+        }
+        {attending === 'no' &&
+          <p className="thankyou-paragraph">
+            We are sorry to hear that you are not available.
+          </p>
+        }
+        <p className="thankyou-links">
+          <Link to="/">Back to Home Page</Link>
+        </p>
       </div>
     </div>
   );
