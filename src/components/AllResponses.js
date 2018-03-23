86e73744b73177d5266c4d9bf7a54e7fb11b5215
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-22 22:23:09 -0400

    set up AllResponses component and data fetch

---
+++ src/components/AllResponses.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class AllResponses extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: '',
+      responses: []
+    };
+  };
+
+  componentDidMount() {
+    this.getResponses();
+  }
+
+  getResponses() {
+    const re = /\?key=(.*)/,
+          key = re.exec(this.props.location.search)[1],
+          URL = `${process.env.BASE_URL}/responses?key=${key}`;
+
+    console.log(URL);
+    axios.get(URL)
+      .then((res) => {
+        this.setState(() => {
+          if (res.data.error) return { error: res.data.error };
+
+          return { responses: data.responses };
+        });
+      })
+      .catch((e) => console.log(e));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+      </div>
+    );
+  }
+};
