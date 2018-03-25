f350eaa2252c4dc92529f647534b7a09330fc940
David Ordovskiy <david.ordovsky@gmail.com>
2018-03-25 19:43:34 -0400

    create separate files for homepage contents

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="box__footer">
+    <p>&copy; 2018 | Made with &hearts; by David Ordovskiy</p>
+    <p>Photos: <a href="https://unsplash.com/@antonmislawsky">Anton Mislawsky</a>
+      , <a href="https://unsplash.com/@itstamaramenzi">Tamara Menzi</a>
+      , <br className="show-for-mobile" /><a href="https://unsplash.com/@tikkho">Tikkho Maciel</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
