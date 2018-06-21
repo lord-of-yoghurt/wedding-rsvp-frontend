c8910583514f1cc435af2134d6a8b4631f75a68a
David Ordovskiy <david.ordovsky@gmail.com>
2018-06-21 07:59:28 -0400

    make our story page responsive

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -5,16 +5,20 @@ const OurStory = () => (
     <div className="our-story__box">
       <p className="our-story__title">Our Story</p>
       <p>
-        <img className="our-story__img--right" src="/images/us-pic-1.jpg" />
+        <img className="our-story__img--right show-for-desktop" src="/images/us-pic-1.jpg" />
         David and I met in the end of 2011 at the beautiful historic building on 57th street near Carnegie Hall, which was the former New York home for Steinway pianos. We were both working for an event that Mannes - the conservatory we both graduated from, hosted. By then, we had both been studying at Mannes for a few years, and it was a school with just a few hundred students. So we were very surprised that we had never seen each other at school before. It wasn’t till later that we concluded that both of us must have been spending a lot of time in practice rooms. :)
       </p>
+
+      <img className="box-layout__img show-for-mobile" src="/images/us-pic-1.jpg" />
 
       <p>Shortly after the New Years of 2012, I was coming back from visiting a friend in Chicago on an early flight, which arrived in New York at something like 5 in the morning. And there was David, waving and smiling at me at the airport meeting point in his winter jacket. He looked very sleepy, but perhaps the happiest in the crowd.</p>
 
       <p>Since then, we spent almost every minute together. We discovered our mutual love for films and we saw hundreds movies together, both old and new. We played lots of music and performed many times together. In fact, one of our projects after the wedding is to record a CD album produced by a professional studio!</p>
 
+      <img className="box-layout__img show-for-mobile" src="/images/us-pic-2.jpg" />
+
       <p>
-        <img className="our-story__img--left" src="/images/us-pic-2.jpg" />
+        <img className="our-story__img--left show-for-desktop" src="/images/us-pic-2.jpg" />
         David flew to Shanghai to meet my parents just 6 months after we started dating. He could only spend 4 days there because of previous engagements. I thought it was too rushed, but he thought that ‘he had to come’. After a very turbulent (David is very much afraid of it) 8 hour flight and the fear of meeting my parents for the first time, his hands were icy cold (and they never are!) when I picked him up at the airport. Nevertheless, he greeted my parents in perfect Shanghainese (a dialect that we speak that’s different than mandarin) that I had taught him before (he obviously had practiced it a lot!) and both of my parents took an immediate liking to him. And that liking only grew over time. :)
       </p>
 
